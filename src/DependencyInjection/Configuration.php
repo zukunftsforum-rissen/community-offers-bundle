fbb84bd814144dededb4a78568ab2f08a0d6abbe
Ernst Jendritzki <ernst.jendritzki@zukunftsforum-rissen.de>
2026-03-13 22:38:27 +0100

    c

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace ZukunftsforumRissen\CommunityOffersBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -13,19 +12,17 @@ final class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('community_offers');
-
-        /** @var ArrayNodeDefinition $rootNode */
-        $rootNode = $treeBuilder->getRootNode();
-
-        $children = $rootNode->children();
-
-        $loggingEnabled = $children->booleanNode('logging_enabled');
-        $loggingEnabled->defaultFalse();
-        $loggingEnabled->end();
-
-        $debugLoggingEnabled = $children->booleanNode('debug_logging_enabled');
-        $debugLoggingEnabled->defaultFalse();
-        $debugLoggingEnabled->end();
+        $treeBuilder
+            ->getRootNode()
+            ->children()
+            ->booleanNode('logging_enabled')
+            ->defaultFalse()
+            ->end()
+            ->booleanNode('debug_logging_enabled')
+            ->defaultFalse()
+            ->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
